6a7b16d2615d2882e83e1db2284cec6c76cb0e0a
Anton <32439368+kuziaa@users.noreply.github.com>
2022-01-24 18:39:07 +0300

    start task 40

--- src/test/java/org/antonkhmarun/LogInTest.java
+++ src/test/java/org/antonkhmarun/LogInTest.java
@@ -1,13 +1,17 @@
 package org.antonkhmarun;
 
-import org.junit.AfterClass;
-import org.junit.Assert;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.antonkhmarun.pages.LoginPage;
+import org.antonkhmarun.pages.MailYandexPage;
+import org.antonkhmarun.pages.PassportYandexPage;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class LogInTest {
 
@@ -16,36 +20,45 @@ public class LogInTest {
     public static MailYandexPage mailYandexPage;
     public static WebDriver driver;
 
-    @BeforeClass
+    @BeforeAll
     public static void setup() {
-
         System.setProperty("webdriver.chrome.driver", ConfProperties.getProperty("chromedriver"));
+    }
+
+    @BeforeEach
+    public void beforeEach() {
         driver = new ChromeDriver();
+        driver.manage().window().fullscreen();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/Login_Password.csv", numLinesToSkip = 1)
+    public void loginTest(String login, String password) throws InterruptedException {
+
         loginPage = new LoginPage(driver);
         passportYandexPage = new PassportYandexPage(driver);
         mailYandexPage = new MailYandexPage(driver);
-        driver.manage().window().fullscreen();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get(ConfProperties.getProperty("loginpage"));
-    }
 
-    @Test
-    public void loginTest() {
+        driver.get(ConfProperties.getProperty("loginpage"));
 
         loginPage.clickLoginBtn();
 
-        passportYandexPage.inputLogin(ConfProperties.getProperty("login"));
+        passportYandexPage.inputLogin(login);
         passportYandexPage.clickLoginBtn();
-        passportYandexPage.inputPassword(ConfProperties.getProperty("password"));
+        passportYandexPage.inputPassword(password);
         passportYandexPage.clickLoginBtn();
 
+        // explicit waiter
+        Thread.sleep(2000);
+        
         String userName = mailYandexPage.getUserName();
 
-        Assert.assertEquals(ConfProperties.getProperty("login"), userName);
+        assertEquals(login, userName);
     }
 
-    @AfterClass
-    public static void tearDown() {
+    @AfterEach
+    public void tearDown() {
         driver.close();
     }
 }
---
+++ src/test/java/org/antonkhmarun/pages/MailYandexPage.java
@@ -0,0 +1,25 @@
+package org.antonkhmarun.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MailYandexPage {
+
+    public WebDriver driver;
+
+    public MailYandexPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(xpath = "//*[@class='user-account user-account_left-name user-account_has-ticker_yes user-account_has-accent-letter_yes count-me legouser__current-account legouser__current-account i-bem']//span[@class='user-account__name']")
+    private WebElement userAccount;
+
+
+    public String getUserName() {
+        String userName = userAccount.getAttribute("innerText");
+        return userName;
+    }
+}
