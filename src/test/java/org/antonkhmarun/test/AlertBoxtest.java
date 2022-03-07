a91ba7cdb2c1c30d33ac1f0892c97241b4328435
Anton <32439368+kuziaa@users.noreply.github.com>
2022-03-07 18:38:24 +0300

    disable all tests except of one.

--- src/test/java/org/antonkhmarun/test/AlertBoxtest.java
+++ src/test/java/org/antonkhmarun/test/AlertBoxtest.java
@@ -1,61 +1,61 @@
-package org.antonkhmarun.test;
-
-import org.antonkhmarun.config.ConfProperties;
-import org.antonkhmarun.pageFactory.JavascriptAlertBoxDemo;
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.Alert;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class AlertBoxtest extends BaseTest{
-
-    JavascriptAlertBoxDemo javascriptAlertBoxDemo;
-
-    @Test
-    public void confirmBoxTextInsideTest() {
-        javascriptAlertBoxDemo = new JavascriptAlertBoxDemo(driver);
-        driver.get(ConfProperties.getProperty("javascriptAlertBoxDemo"));
-        javascriptAlertBoxDemo.clickJavaScriptConfirmBoxBtn();
-
-        wait.until(ExpectedConditions.alertIsPresent());
-
-        Alert alert = driver.switchTo().alert();
-        String textOnAlert = alert.getText();
-        assertEquals("Press a button!", textOnAlert, "Confirm box has correct text inside");
-        alert.accept();
-    }
-
-    @Test
-    public void confirmBoxMsgAfterAcceptTest() {
-        javascriptAlertBoxDemo = new JavascriptAlertBoxDemo(driver);
-        driver.get(ConfProperties.getProperty("javascriptAlertBoxDemo"));
-        javascriptAlertBoxDemo.clickJavaScriptConfirmBoxBtn();
-
-        wait.until(ExpectedConditions.alertIsPresent());
-
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-
-        assertEquals("You pressed OK!", javascriptAlertBoxDemo.getJavaScriptConfirmBoxMsg(),
-                "Confirm box shows correct msg");
-    }
-
-    @Test
-    public void alertBoxMsgAfterAcceptTest() {
-        String msg = "Anton";
-
-        javascriptAlertBoxDemo = new JavascriptAlertBoxDemo(driver);
-        driver.get(ConfProperties.getProperty("javascriptAlertBoxDemo"));
-        javascriptAlertBoxDemo.clickJavaScriptAlertBoxBtn();
-
-        wait.until(ExpectedConditions.alertIsPresent());
-
-        Alert alert = driver.switchTo().alert();
-        alert.sendKeys(msg);
-        alert.accept();
-
-        assertEquals("You have entered '" + msg + "' !", javascriptAlertBoxDemo.getJavaScriptAlertBoxMsg(),
-                "Alert box shows correct msg");
-    }
-}
+//package org.antonkhmarun.test;
+//
+//import org.antonkhmarun.config.ConfProperties;
+//import org.antonkhmarun.pageFactory.JavascriptAlertBoxDemo;
+//import org.junit.jupiter.api.*;
+//import org.openqa.selenium.Alert;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class AlertBoxtest extends BaseTest{
+//
+//    JavascriptAlertBoxDemo javascriptAlertBoxDemo;
+//
+//    @Test
+//    public void confirmBoxTextInsideTest() {
+//        javascriptAlertBoxDemo = new JavascriptAlertBoxDemo(driver);
+//        driver.get(ConfProperties.getProperty("javascriptAlertBoxDemo"));
+//        javascriptAlertBoxDemo.clickJavaScriptConfirmBoxBtn();
+//
+//        wait.until(ExpectedConditions.alertIsPresent());
+//
+//        Alert alert = driver.switchTo().alert();
+//        String textOnAlert = alert.getText();
+//        assertEquals("Press a button!", textOnAlert, "Confirm box has correct text inside");
+//        alert.accept();
+//    }
+//
+//    @Test
+//    public void confirmBoxMsgAfterAcceptTest() {
+//        javascriptAlertBoxDemo = new JavascriptAlertBoxDemo(driver);
+//        driver.get(ConfProperties.getProperty("javascriptAlertBoxDemo"));
+//        javascriptAlertBoxDemo.clickJavaScriptConfirmBoxBtn();
+//
+//        wait.until(ExpectedConditions.alertIsPresent());
+//
+//        Alert alert = driver.switchTo().alert();
+//        alert.accept();
+//
+//        assertEquals("You pressed OK!", javascriptAlertBoxDemo.getJavaScriptConfirmBoxMsg(),
+//                "Confirm box shows correct msg");
+//    }
+//
+//    @Test
+//    public void alertBoxMsgAfterAcceptTest() {
+//        String msg = "Anton";
+//
+//        javascriptAlertBoxDemo = new JavascriptAlertBoxDemo(driver);
+//        driver.get(ConfProperties.getProperty("javascriptAlertBoxDemo"));
+//        javascriptAlertBoxDemo.clickJavaScriptAlertBoxBtn();
+//
+//        wait.until(ExpectedConditions.alertIsPresent());
+//
+//        Alert alert = driver.switchTo().alert();
+//        alert.sendKeys(msg);
+//        alert.accept();
+//
+//        assertEquals("You have entered '" + msg + "' !", javascriptAlertBoxDemo.getJavaScriptAlertBoxMsg(),
+//                "Alert box shows correct msg");
+//    }
+//}
--- src/test/java/org/antonkhmarun/test/PageObjectPatternLoginTest.java
+++ src/test/java/org/antonkhmarun/test/PageObjectPatternLoginTest.java
@@ -1,50 +1,50 @@
-package org.antonkhmarun.test;
-
-import org.antonkhmarun.config.ConfProperties;
-import org.antonkhmarun.pages.MailYandex;
-import org.antonkhmarun.pages.MailYandexLogin;
-import org.antonkhmarun.pages.MailYandexPassword;
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.io.IOException;
-import java.time.Duration;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-public class PageObjectPatternLoginTest extends BaseTest {
-
-    public static MailYandexLogin mailYandexLogin;
-    public static MailYandexPassword mailYandexPassword;
-    public static MailYandex mailYandex;
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/Login_Password.csv", numLinesToSkip = 2)
-    public void loginTest(String login, String password) throws IOException {
-        System.out.println(login);
-        System.out.println(password);
-        mailYandexLogin = new MailYandexLogin(driver);
-        mailYandexPassword = new MailYandexPassword(driver);
-        mailYandex = new MailYandex(driver);
-
-        driver.get(ConfProperties.getProperty("loginPage"));
-
-        mailYandexLogin.pushLoginBtn();
-
-        mailYandexPassword.inputLogin(login);
-        mailYandexPassword.clickLoginBtn();
-        mailYandexPassword.inputPassword(password);
-        mailYandexPassword.clickLoginBtn();
-
-        wait.until(ExpectedConditions.visibilityOf(mailYandex.getUserAccount()));
-        String userName = mailYandex.getUserName();
-        assertEquals(login, userName, "Correct login name is displayed");
-
-        takeScreenShot(prepareFilePath("pageObjectPatternYandexMail"));
-    }
-}
+//package org.antonkhmarun.test;
+//
+//import org.antonkhmarun.config.ConfProperties;
+//import org.antonkhmarun.pages.MailYandex;
+//import org.antonkhmarun.pages.MailYandexLogin;
+//import org.antonkhmarun.pages.MailYandexPassword;
+//import org.junit.jupiter.api.*;
+//import org.junit.jupiter.params.ParameterizedTest;
+//import org.junit.jupiter.params.provider.CsvFileSource;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.support.ui.ExpectedConditions;
+//import org.openqa.selenium.support.ui.WebDriverWait;
+//
+//import java.io.IOException;
+//import java.time.Duration;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//public class PageObjectPatternLoginTest extends BaseTest {
+//
+//    public static MailYandexLogin mailYandexLogin;
+//    public static MailYandexPassword mailYandexPassword;
+//    public static MailYandex mailYandex;
+//
+//    @ParameterizedTest
+//    @CsvFileSource(resources = "/Login_Password.csv", numLinesToSkip = 2)
+//    public void loginTest(String login, String password) throws IOException {
+//        System.out.println(login);
+//        System.out.println(password);
+//        mailYandexLogin = new MailYandexLogin(driver);
+//        mailYandexPassword = new MailYandexPassword(driver);
+//        mailYandex = new MailYandex(driver);
+//
+//        driver.get(ConfProperties.getProperty("loginPage"));
+//
+//        mailYandexLogin.pushLoginBtn();
+//
+//        mailYandexPassword.inputLogin(login);
+//        mailYandexPassword.clickLoginBtn();
+//        mailYandexPassword.inputPassword(password);
+//        mailYandexPassword.clickLoginBtn();
+//
+//        wait.until(ExpectedConditions.visibilityOf(mailYandex.getUserAccount()));
+//        String userName = mailYandex.getUserName();
+//        assertEquals(login, userName, "Correct login name is displayed");
+//
+//        takeScreenShot(prepareFilePath("pageObjectPatternYandexMail"));
+//    }
+//}
