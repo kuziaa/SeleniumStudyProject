3de0b325918f2febd6215c21cd06e80d840fa7d0
Anton <32439368+kuziaa@users.noreply.github.com>
2022-03-24 17:33:05 +0300

    configure tests to work with sauselab

--- src/test/java/org/antonkhmarun/test/BaseTest.java
+++ src/test/java/org/antonkhmarun/test/BaseTest.java
@@ -5,14 +5,13 @@ import org.antonkhmarun.support.TestResultWatcher;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.Platform;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
@@ -21,40 +20,55 @@ public class BaseTest {
 
     public static WebDriver driver;
     public static WebDriverWait wait;
-    public static DesiredCapabilities desiredCapabilitiesChrome;
-    public static DesiredCapabilities desiredCapabilitiesFirefox;
+    public static String sauceUrl;
+    public static MutableCapabilities sauceOptions;
 
     @BeforeAll
-    public static void setup() {
-        desiredCapabilitiesChrome = new DesiredCapabilities();
-        desiredCapabilitiesChrome.setBrowserName("chrome");
-        desiredCapabilitiesChrome.setPlatform(Platform.WIN10);
-
-        desiredCapabilitiesFirefox = new DesiredCapabilities();
-        desiredCapabilitiesFirefox.setBrowserName("firefox");
-        desiredCapabilitiesFirefox.setPlatform(Platform.WIN10);
+    public static void setup() throws Exception {
+        sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", ConfProperties.getProperty("sauceUserName"));
+        sauceOptions.setCapability("accessKey", ConfProperties.getProperty("sauceAccessKey"));
+        sauceUrl = ConfProperties.getProperty("sauceUrl");
     }
 
-    // Default realization
     @BeforeEach
-    public void beforeEach() throws MalformedURLException {
-        driver = new RemoteWebDriver(new URL("http://10.10.8.119:4444/wd/hub"), desiredCapabilitiesChrome);
+    public void beforeEach() throws Exception {
+        driver = getDriverByEnvironment();
         driver.manage().window().maximize();
+        driver.manage().window().fullscreen();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         wait = new WebDriverWait(driver, Duration.ofSeconds(20));
     }
 
-    public void setupDriver(String browser) throws MalformedURLException {
-        switch (browser) {
-            case "chrome":
-                driver = new RemoteWebDriver(new URL("http://10.10.8.119:4444/wd/hub"), desiredCapabilitiesChrome);
-                break;
-            case "firefox":
-                driver = new RemoteWebDriver(new URL("http://10.10.8.119:4444/wd/hub"), desiredCapabilitiesFirefox);
-        }
+    private RemoteWebDriver getDriverByEnvironment() throws Exception {
+        String browserName = ConfProperties.getProperty("browserName");
+        String browserVersion = ConfProperties.getProperty("browserVersion");
+        String platformName = ConfProperties.getProperty("platformName");
 
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        switch (browserName) {
+            case "chrome": {
+                ChromeOptions browserOptions = new ChromeOptions();
+                browserOptions.setPlatformName(platformName);
+                browserOptions.setBrowserVersion(browserVersion);
+                browserOptions.setCapability("sauce:options", sauceOptions);
+                return new RemoteWebDriver(new URL(sauceUrl), browserOptions);
+            }
+            case "firefox": {
+                FirefoxOptions browserOptions = new FirefoxOptions();
+                browserOptions.setPlatformName(platformName);
+                browserOptions.setBrowserVersion(browserVersion);
+                browserOptions.setCapability("sauce:options", sauceOptions);
+                return new RemoteWebDriver(new URL(sauceUrl), browserOptions);
+            }
+            case "edge": {
+                EdgeOptions browserOptions = new EdgeOptions();
+                browserOptions.setPlatformName(platformName);
+                browserOptions.setBrowserVersion(browserVersion);
+                browserOptions.setCapability("sauce:options", sauceOptions);
+                return new RemoteWebDriver(new URL(sauceUrl), browserOptions);
+            }
+            default:
+                throw new Exception("Unexpected environment");
+        }
     }
 }
